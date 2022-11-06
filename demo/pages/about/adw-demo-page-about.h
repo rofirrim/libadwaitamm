5c0246b8b0c0f80fea94243f5e2465aef672b5f0
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-11-06 11:47:57 +0100

    Manually wrap the class so we can use gtk templates
    It is a bit unfortunate this cannot be used from just a derived
    object. The C++ constructor would get in the way.

--- demo/pages/about/adw-demo-page-about.h
+++ demo/pages/about/adw-demo-page-about.h
@@ -1,15 +1,5 @@
 #pragma once
 
-#include <adwaita.h>
-
-G_BEGIN_DECLS
-
-#define ADW_TYPE_DEMO_PAGE_ABOUT (adw_demo_page_about_get_type())
-
-G_DECLARE_FINAL_TYPE (AdwDemoPageAbout, adw_demo_page_about, ADW, DEMO_PAGE_ABOUT, AdwBin)
-
-G_END_DECLS
-
 #include <libadwaitamm.h>
 #include "namedclass.h"
 
