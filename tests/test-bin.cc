d3e6fe77028bf6890be6abbe5e185ab67455fd5b
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-04 15:55:32 +0100

    Add test-bin.cc

---
+++ tests/test-bin.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Author: Alexander Mikhaylenko <alexander.mikhaylenko@puri.sm>
+ */
+
+#include <libadwaitamm.h>
+#include <libadwaitamm/init.h> // Adw::init
+
+int notified;
+
+static void notify_cb(void) { notified++; }
+
+static void test_adw_bin_child(void) {
+  Adw::Bin bin;
+
+  notified = 0;
+  bin.property_child().signal_changed().connect(sigc::ptr_fun(notify_cb));
+
+  Gtk::Widget *widget = bin.get_property<Gtk::Widget *>("child");
+  g_assert_null(widget);
+
+  bin.set_child(nullptr);
+  g_assert(notified == 0);
+
+  widget = Gtk::make_managed<Gtk::Button>();
+  bin.set_child(widget);
+  g_assert(bin.get_child() == widget);
+  g_assert(notified == 1);
+
+  bin.set_property<Gtk::Widget *>("child", nullptr);
+  g_assert(notified == 2);
+}
+
+int main(int argc, char *argv[]) {
+  gtk_test_init(&argc, &argv, NULL);
+  Adw::init();
+
+  g_test_add_func("/Adwaita/Bin/child", test_adw_bin_child);
+
+  return g_test_run();
+}
