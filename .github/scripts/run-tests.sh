e353a1e58accccb1a225466b45c18fbc7cd21402
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-04 10:42:22 +0100

    Add some more logging

--- .github/scripts/run-tests.sh
+++ .github/scripts/run-tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+
 # Script based on gtk's testing scripts
+
+env
 
 export XDG_RUNTIME_DIR="$(mktemp -p $(pwd) -d xdg-runtime-XXXXXX)"
 weston --backend=headless-backend.so --socket=wayland-5 --idle-time=0 &
