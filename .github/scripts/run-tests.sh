8b470dc78868e55daa6552d0dcff70fabe0847f3
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-04 09:54:28 +0100

    Run tests under weston compositor

---
+++ .github/scripts/run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Script based on gtk's testing scripts
+
+export XDG_RUNTIME_DIR="$(mktemp -p $(pwd) -d xdg-runtime-XXXXXX)"
+weston --backend=headless-backend.so --socket=wayland-5 --idle-time=0 &
+compositor=$!
+
+export WAYLAND_DISPLAY=wayland-5
+export GDK_BACKEND=wayland
+
+meson test
+result=$?
+
+kill ${compositor}
+
+exit ${result}
