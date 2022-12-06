256b1eec1b55dd8307cc27ce6aa58b399cfbab8d
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-06 17:34:34 +0100

    Add test-carousel-indicator-lines.cc

---
+++ tests/test-carousel-indicator-lines.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2020 Alexander Mikhaylenko <alexm@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <libadwaitamm.h>
+#include <libadwaitamm/init.h> // Adw::init
+
+int notified;
+static void notify_cb() { notified++; }
+
+static void test_adw_carousel_indicator_lines_carousel(void) {
+    Adw::CarouselIndicatorLines lines;
+    Adw::Carousel carousel;
+
+  notified = 0;
+  lines.property_carousel().signal_changed().connect(sigc::ptr_fun(notify_cb));
+
+  g_assert_null(lines.get_carousel());
+  g_assert(notified == 0);
+
+  lines.set_carousel(&carousel);
+  g_assert(lines.get_carousel()->gobj() == carousel.gobj());
+  g_assert(notified == 1);
+
+  lines.set_carousel(nullptr);
+  g_assert_null(lines.get_carousel());
+  g_assert(notified == 2);
+}
+
+int main(int argc, char *argv[]) {
+  gtk_test_init(&argc, &argv, NULL);
+  Adw::init();
+
+  g_test_add_func("/Adwaita/CarouselInidicatorLines/carousel",
+                  test_adw_carousel_indicator_lines_carousel);
+  return g_test_run();
+}
