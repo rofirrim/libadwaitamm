27507adf78f11c81064bdbb4f04bc1b9306e11d1
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-27 10:27:39 +0100

    Add test-preferences-group.cc

---
+++ tests/test-preferences-group.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2019 Purism SPC
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include <libadwaitamm.h>
+#include <libadwaitamm/init.h> // Adw::init
+
+static void test_adw_preferences_group_add_remove(void) {
+  Adw::PreferencesGroup group;
+
+  Adw::PreferencesRow *row = Gtk::make_managed<Adw::PreferencesRow>();
+  group.add(row);
+
+  Gtk::Switch *widget = Gtk::make_managed<Gtk::Switch>();
+  group.add(widget);
+
+  g_assert(G_TYPE_CHECK_INSTANCE_TYPE(
+      gtk_widget_get_parent(GTK_WIDGET(row->gobj())), GTK_TYPE_LIST_BOX));
+  g_assert(G_TYPE_CHECK_INSTANCE_TYPE(
+      gtk_widget_get_parent(GTK_WIDGET(widget->gobj())), GTK_TYPE_BOX));
+
+  group.remove(row);
+  group.remove(widget);
+}
+
+static void test_adw_preferences_group_title(void) {
+  Adw::PreferencesGroup group;
+
+  g_assert(group.get_title() == "");
+
+  group.set_title("Dummy title");
+  g_assert(group.get_title() == "Dummy title");
+
+  group.set_title("");
+  g_assert(group.get_title() == "");
+}
+
+static void test_adw_preferences_group_description(void) {
+  Adw::PreferencesGroup group;
+
+  g_assert(group.get_description() == "");
+
+  group.set_description("Dummy description");
+  g_assert(group.get_description() == "Dummy description");
+
+  group.set_description("");
+  g_assert(group.get_description() == "");
+}
+
+int main(int argc, char *argv[]) {
+  gtk_test_init(&argc, &argv, NULL);
+  Adw::init();
+
+  g_test_add_func("/Adwaita/PreferencesGroup/add_remove",
+                  test_adw_preferences_group_add_remove);
+  g_test_add_func("/Adwaita/PreferencesGroup/title",
+                  test_adw_preferences_group_title);
+  g_test_add_func("/Adwaita/PreferencesGroup/description",
+                  test_adw_preferences_group_description);
+
+  return g_test_run();
+}
