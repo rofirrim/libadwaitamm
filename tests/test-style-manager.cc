2084dd2ac9159bc18c8353d938009fbe80e0c33c
Roger Ferrer Ibanez <rofirrim@gmail.com>
2022-12-27 15:36:53 +0100

    Add test-style-manager.cc
    The original C code uses internal classes from libadwaita that
    are not meant to be used externally, so those tests are not included.

---
+++ tests/test-style-manager.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Author: Alexander Mikhaylenko <alexander.mikhaylenko@puri.sm>
+ */
+
+#include <libadwaitamm.h>
+#include <libadwaitamm/init.h> // Adw::init
+
+int notified;
+
+static void notify_cb() { notified++; }
+
+static void test_adw_style_manager_color_scheme(void) {
+  Glib::RefPtr<Adw::StyleManager> manager = Adw::StyleManager::get_default();
+  Adw::ColorScheme color_scheme;
+
+  notified = 0;
+  sigc::connection conn =
+      manager->property_color_scheme().signal_changed().connect(
+          sigc::ptr_fun(notify_cb));
+
+  color_scheme = manager->get_property<Adw::ColorScheme>("color-scheme");
+  g_assert(color_scheme == Adw::ColorScheme::DEFAULT);
+  g_assert(notified == 0);
+
+  manager->set_color_scheme(Adw::ColorScheme::DEFAULT);
+  g_assert(notified == 0);
+
+  manager->set_color_scheme(Adw::ColorScheme::PREFER_DARK);
+  color_scheme = manager->get_property<Adw::ColorScheme>("color-scheme");
+  g_assert(color_scheme == Adw::ColorScheme::PREFER_DARK);
+  g_assert(notified == 1);
+
+  manager->set_property<Adw::ColorScheme>("color-scheme",
+                                          Adw::ColorScheme::PREFER_LIGHT);
+  g_assert(manager->get_color_scheme() == Adw::ColorScheme::PREFER_LIGHT);
+  g_assert(notified == 2);
+
+  conn.disconnect();
+  manager->set_color_scheme(Adw::ColorScheme::DEFAULT);
+}
+
+int main(int argc, char *argv[]) {
+  gtk_test_init(&argc, &argv, NULL);
+  Adw::init();
+
+  g_test_add_func("/Adwaita/StyleManager/color_scheme",
+                  test_adw_style_manager_color_scheme);
+
+  return g_test_run();
+}
